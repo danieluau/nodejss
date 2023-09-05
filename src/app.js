57db520631106b9a9ca914ced7fb292bdfd24649
danieluau <daniellealuau@gmail.com>
2023-09-04 21:01:23 -0300

    select por id feita

--- src/app.js
+++ src/app.js
@@ -34,9 +34,17 @@ app.get('/musicos', (req, res)=>{
 } )
 
 app.get('/musicos/:id', (req, res) => {
-    // let index = req.params.id;
-    // console.log(index);
-    res.json(buscarPorId(req.params.id))
+    const id = req.params.id;
+    const sql = "SELECT * FROM musicos WHERE id_musicos=?;"
+    conexao.query(sql, id, (error, result) => {
+        const linha = result[0]
+        if(error){
+          console.log(error)
+          res.status(404).json({'error': error})
+        }else{
+          res.status(200).json(linha)
+        }
+    })
 })
 app.post('/musicos', (req, res)=>{
   musicos.push(req.body)
