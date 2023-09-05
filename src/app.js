70887b4d1981853286a64dc0757748dbab999a81
danieluau <daniellealuau@gmail.com>
2023-09-05 17:31:45 -0300

    refatorado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import conexao from '../infra/conexao.js'
+
+import selecaoController from './app/controllers/selecaoController.js';
+
 const app = express();
 
 // indicar para o express ler body com json
@@ -19,72 +21,15 @@ function buscaIndice(id) {
 
 // criando rotas    
 
-app.get('/musicos', (req, res)=>{
-
-  //res.status(200).send(musicos)
-  const sql = "SELECT * FROM musicos;"
-  conexao.query(sql, (error, result) => {
-      if(error){
-        console.log(error)
-        res.status(404).json({'error': error})
-      }else{
-        res.status(200).json(result)
-      }
-  })
-} )
-
-app.get('/musicos/:id', (req, res) => {
-    const id = req.params.id;
-    const sql = "SELECT * FROM musicos WHERE id_musicos=?;"
-    conexao.query(sql, id, (error, result) => {
-        const linha = result[0]
-        if(error){
-          console.log(error)
-          res.status(404).json({'error': error})
-        }else{
-          res.status(200).json(linha)
-        }
-    })
-})
-
-app.post('/musicos', (req, res)=>{
-  const musico = req.body
-  const sql = "INSERT INTO musicos SET ?;"
-  conexao.query(sql, musico, (error, result) => {
-      if(error){
-        console.log(error)
-        res.status(400).json({'error': error})
-      }else{
-        res.status(201).json(result)
-      }
-  })
-} )
-
-app.delete('/musicos/:id', (req, res) =>{
-  const id = req.params.id;
-  const sql = "DELETE FROM musicos WHERE id_musicos=?;"
-  conexao.query(sql, id, (error, result) => {
-      if(error){
-        console.log(error)
-        res.status(404).json({'error': error})
-      }else{
-        res.status(200).json(result)
-      }
-  })
-})
-
-app.put('/musicos/:id', (req, res) =>{
-  const id = req.params.id
-  const musico = req.body
-  const sql = "UPDATE musicos SET ? WHERE id_musicos=?;"
-  conexao.query(sql, [musico, id], (error, result) => {
-      if(error){
-        console.log(error)
-        res.status(400).json({'error': error})
-      }else{
-        res.status(200).json(result)
-      }
-  })
-})
+// listar todos
+app.get('/musicos', selecaoController.index);
+// listar por id
+app.get('/musicos/:id', selecaoController.show)
+// criar 
+app.post('/musicos',  selecaoController.store)
+// deletar
+app.delete('/musicos/:id', selecaoController.delete)
+// atualizar
+app.put('/musicos/:id', selecaoController.update)
 
 export default app;
