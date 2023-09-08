afca380b8c491d1345b026af3bba5019d2e59904
Daniel Araujo Leal <daniellealuau@gmail.com>
2023-09-08 17:20:33 -0300

    refatorado para MVC

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -6,8 +6,8 @@ const conexao = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: '02082001@UaU',
-    database: 'bdmusicos'
+    password: '',
+    database: ''
 });
 
 conexao.connect
